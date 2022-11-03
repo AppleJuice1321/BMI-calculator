4ea4a5be201cde050f3a5b3da2e82f9cefbc4b7d
AppleJuice1321 <117173708+AppleJuice1321@users.noreply.github.com>
2022-11-03 20:56:48 +0100

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,11 @@
+function BeregnBMI() {
+    //Bereign BMI som vægten div, med højden i 2. potens
+    var height=document.getElementById("Height").value;
+    var weight=document.getElementById("weight").value;
+    var BMI=(parseFloat(Weight)/(parseFloat(Height)*parseFloat(Height))).toPrecision(3));
+    document.getElementById("result").innerHTML = ("din BMI er: " +BMI)
+
+    if (BMI >= 20,25 >= BMI)
+    if (BMI > 25)
+    if (20 > BMI)
+}
